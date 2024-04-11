81b133c5a9a9a2e98b59ac873b8eac161c04bb58
yite <coastq22889@icloud.com>
2024-04-11 15:13:11 +0800

    main

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db import create_start_app_handler, create_stop_app_handler
 from app.api.routers import auth_router, scheduler_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows only specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.add_event_handler("startup", create_start_app_handler(app))
 app.add_event_handler("shutdown", create_stop_app_handler(app))
