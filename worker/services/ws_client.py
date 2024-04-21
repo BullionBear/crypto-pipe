8fc325972a38b5ed0ce22ab5c97360d79b0b4b0b
Ubuntu <ubuntu@ip-172-31-33-183.ap-northeast-1.compute.internal>
2024-04-21 13:22:46 +0000

    update utils

--- worker/services/ws_client.py
+++ worker/services/ws_client.py
@@ -1,6 +1,8 @@
+from typing import Dict, Any
 import asyncio
 import websockets
 import loguru
+import uuid
 import pipe.command as cmd
 
 logger = loguru.logger
@@ -19,16 +21,35 @@ class WebSocketClient:
         try:
             while True:
                 message = await websocket.recv()
-                await self.on_message(message)
+                await self.on_message(message, websocket)
         except websockets.ConnectionClosed:
             await self.on_close()
         except Exception as e:
             await self.on_error(e)
 
-    async def on_message(self, message_str):
+    async def on_message(self, message_str, websocket):
         logger.info(f"Received message: {message_str}")
         message = cmd.resolve_command(message_str)
+        command = message["cmd"]
+        logger.info(f"Receive {command} with {message["id"]}")
+        if command == cmd.MASTER_CONNECTION_ESTABLISHED_ACK:
+            await self.on_ack(message)
+        elif command == cmd.MASTER_CREATE_TASK:
+            await self.on_create_task(message, websocket)
+    
+    async def on_ack(self, message: Dict[str, Any]):
+        pass
         
+    
+    async def on_create_task(self, message: Dict[str, Any], websocket):
+        response = cmd.make_command(cmd.WORKER_CREATE_TASK_ACK, message["id"], dict())
+        await websocket.send(response)
+        task_name = message["task"]
+        args = message["args"]
+        kwargs = message["kwargs"]
+        logger.info(f"Start running {task_name}({args}, {kwargs})")
+        reply = cmd.make_command(cmd.WORKER_TASK_DONE, uuid.uuid4().hex, dict())
+        await websocket.send(reply)
 
     async def on_error(self, error):
         logger.info(f"Encountered error: {error}")
@@ -37,8 +58,9 @@ class WebSocketClient:
         logger.info("Connection closed")
 
     async def on_open(self, websocket):
+        message = cmd.make_command(cmd.WORKER_CONNECTION_ESTABLISH, uuid.uuid4().hex, dict())
         logger.info("Connection opened")
-        await websocket.send("Hello, Server!")
+        await websocket.send(message)
 
 # Function to run the WebSocket client
 async def run_client():
