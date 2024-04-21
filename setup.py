fb3905ed84549215877596858cb7088071ea8001
Ubuntu <ubuntu@ip-172-31-33-183.ap-northeast-1.compute.internal>
2024-04-21 13:48:46 +0000

    upgrade pipe

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pipe',
-    version='0.0.5',
+    version='0.0.6',
     description='Shared library for crypto-pipe project',
     author='Yi Te',
     author_email='coastq22889@icloud.com',
